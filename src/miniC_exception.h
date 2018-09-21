5e0d3a717fbb04f9acfce9ae602509a37325eb71
rsy56640 <851911672@qq.com>
2018-09-21 14:52:35 +0800

    ...

---
+++ src/miniC_exception.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <exception>
+#include <string>
+#include <iostream>
+
+namespace Mini_C
+{
+	class MiniC_Base_Exception
+	{
+	public:
+		void printException() const { this->printEx(); }
+	private:
+		virtual void printEx() const = 0;
+	};
+
+
+	/*
+	 * Universal Exception class.
+	 *     display specific error message, line num, and position.
+	 */
+	class MiniC_Universal_Exception : public MiniC_Base_Exception
+	{
+		const std::string _msg;
+		const std::size_t _line, _position;
+		virtual void printEx() const { std::cout << (*this) << std::endl; }
+	public:
+		MiniC_Universal_Exception(const std::string& msg, std::size_t line, std::size_t position)
+			:_msg(msg), _line(line), _position(position) {}
+		MiniC_Universal_Exception(std::string&& msg, std::size_t line, std::size_t position)
+			:_msg(std::move(msg)), _line(line), _position(position) {}
+		friend std::ostream& operator<<(std::ostream& os, const MiniC_Universal_Exception& e)
+		{
+			os << e._msg << " in the line: " << e._line << ", at position: " << e._position;
+			return os;
+		}
+	};
+
+}
